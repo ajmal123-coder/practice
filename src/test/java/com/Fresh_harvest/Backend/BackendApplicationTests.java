612502e27f5178928c03f1adf1907ee85712595c
ajmal123-coder <ajmalsadhath@gmail.com>
2025-06-06 12:13:50 +0530

    first commit

---
+++ src/test/java/com/Fresh_harvest/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fresh_harvest.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
